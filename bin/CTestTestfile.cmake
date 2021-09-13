df07fa8b8867691df25b6f24ad3eba1f3de98fa0
Siinthd <Siinthd@protonmail.com>
2021-09-13 14:59:24 +0300

    delete bin

--- bin/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/siinthddev/CppProjects/LabsOtys/WConanExample
-# Build directory: /home/siinthddev/CppProjects/LabsOtys/WConanExample/bin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_filter "test_filter")
-set_tests_properties(test_filter PROPERTIES  _BACKTRACE_TRIPLES "/home/siinthddev/CppProjects/LabsOtys/WConanExample/CMakeLists.txt;58;add_test;/home/siinthddev/CppProjects/LabsOtys/WConanExample/CMakeLists.txt;0;")
